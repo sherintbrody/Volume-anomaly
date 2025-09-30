ac24f64881bbcba2cfd908aae69d919367da5dfe
sherintbrody <42165562+sherintbrody@users.noreply.github.com>
2025-09-30 22:19:45 +0530

    Update home(recovery mode).py

--- pages/home(recovery mode).py
+++ pages/home(recovery mode).py
@@ -67,7 +67,7 @@ st.markdown(BADGE_CSS, unsafe_allow_html=True)
 
 # ====== CONFIG ======
 # IMPORTANT: Move these to environment variables or .streamlit/secrets.toml
-API_KEY = "898edbf002db3dfc4becb7bc1940e758-fe041453e27566c239f77c9d2754ef50"
+API_KEY = "bb87dd2bf7964ed9d84b5a8d7cd6b775-06058183a6307a0550c35b109c8cd94d"
 ACCOUNT_ID = "101-004-37091392-001"
 BASE_URL = "https://api-fxpractice.oanda.com/v3"
 
