1c4c0ebf255f30dfb95e5b16ae4713ba57df1a0f
sherintbrody <42165562+sherintbrody@users.noreply.github.com>
2025-10-02 19:36:43 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -506,7 +506,7 @@ with st.sidebar.expander("🔔 **Alerts**", expanded=True):
     
     st.slider(
         "Threshold Multiplier",
-        min_value=1.0,
+        min_value=0.1,
         max_value=3.0,
         step=0.1,
         value=1.618,
