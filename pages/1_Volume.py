3932fdb7ef42991be624997c03ed899d2affaec0
sherintbrody <sherintmundackal@gmail.com>
2025-11-13 12:44:50 +0530

    Update 1_Volume.py

--- pages/1_Volume.py
+++ pages/1_Volume.py
@@ -131,7 +131,7 @@ if "enable_telegram_alerts" not in st.session_state:
 if "candle_size" not in st.session_state:
     st.session_state.candle_size = "15 min"
 if "alert_multiplier" not in st.session_state:
-    st.session_state.alert_multiplier = 2.0
+    st.session_state.alert_multiplier = 1.618
 
 # Instrument Selection
 st.sidebar.multiselect(
